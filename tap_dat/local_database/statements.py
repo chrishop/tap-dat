248a7697ccf0e11b5581df4d020577571d9d65a9
chrishop <christopher.hopkins@bbc.co.uk>
2019-12-12 09:39:15 +0000

    made the statments actually private

--- tap_dat/local_database/statements.py
+++ tap_dat/local_database/statements.py
@@ -4,26 +4,26 @@ class Statements:
    
     @staticmethod
     def create(table_name: str, columns: List[dict]) -> str:
-        columns_as_string = Statements._generate_name_type_string(columns)
+        columns_as_string = Statements.__generate_name_type_string(columns)
         return f"CREATE TABLE {table_name} ({columns_as_string})"
     
     @staticmethod
     def insert(table_name: str, column_names: List[str]) -> str:
-        separated_column_names = Statements._comma_separate(column_names)
-        insert_values = Statements._generate_insert_values(column_names)
+        separated_column_names = Statements.__comma_separate(column_names)
+        insert_values = Statements.__generate_insert_values(column_names)
         
         return f"INSERT INTO {table_name} ({separated_column_names}) VALUES ({insert_values})"
     
     @staticmethod
-    def _generate_name_type_string(columns: List[dict]) -> str:
+    def __generate_name_type_string(columns: List[dict]) -> str:
         return ",".join(list(map(lambda x: f"{x['name']} {x['type']}", columns)))
     
     @staticmethod
-    def _comma_separate(columns_names: List[str]) -> str:
+    def __comma_separate(columns_names: List[str]) -> str:
         return ",".join(columns_names)
     
     @staticmethod
-    def _generate_insert_values(column_names: List[str]) -> str:
+    def __generate_insert_values(column_names: List[str]) -> str:
         return ",".join(list(map(
             lambda column: f"%({column})s",
             column_names)))
