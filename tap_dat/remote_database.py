740729bd949e653c8996cd3ad7c5eece4bc80c6b
chrishop <christopher.hopkins@bbc.co.uk>
2019-12-14 23:17:22 +0000

    high level functions for remote database

--- tap_dat/remote_database.py
+++ tap_dat/remote_database.py
@@ -16,11 +16,24 @@ class RemoteDatabase:
     def __init__(self, url: str):
         self.client = TapPlus(url)
         
+    def get_batch(self, table_name: str, id_name: str,
+                  min: int, max: int, columns='*') -> List[dict]:
+        return self.query(f"SELECT {columns} FROM {table_name} \
+                           WHERE {id_name}>={min} \
+                           AND {id_name}<{max} \
+                           ORDER BY {id_name}")
+
+    def get_schema(self, table_name, columns="*"):
+        return self.query(f"SELECT {columns} \
+                    FROM TAP_SCHEMA.columns \
+                    WHERE table_name='{table_name}' \
+                    ORDER BY column_order")
+
     def query(self, adql_query: str) -> List[dict]:
         job = self.client.launch_job_async(adql_query)
         job.wait_for_job_end()
         return RemoteDatabase.__table_to_dicts(job.get_results())
-    
+
     @staticmethod
     def __table_to_dicts(table) -> List[dict]:
         return list(
--- test/test_remote_database.py
+++ test/test_remote_database.py
@@ -5,31 +5,55 @@ import numpy as np
 class TestRemoteDatabase(unittest.TestCase):
     
     # more of an intergration test
+
+    def setUp(self):
+        self.skymapper = RemoteDatabase('http://api.skymapper.nci.org.au/public/tap')
+
     def test_query_with_table_data(self):
-        skymapper = RemoteDatabase('http://api.skymapper.nci.org.au/public/tap')
-        
-        result = skymapper.query('SELECT object_id, raj2000, dej2000, apass_dist \
+
+
+        result = self.skymapper.query('SELECT object_id, raj2000, dej2000, apass_dist \
                                   FROM dr1.master WHERE object_id=1')
-        
+
         expected = [{ 'object_id': 1,
                       'raj2000': 315.001047, 
                       'dej2000': -35.252904, 
                       'apass_dist': 'NULL'}]
-        
+
         self.assertEqual(result,expected)
-        
+
     def test_query_with_schema_data(self):
-        skymapper = RemoteDatabase('http://api.skymapper.nci.org.au/public/tap')
-        
-        result = skymapper.query("SELECT column_name, column_order, description, unit \
+
+        result = self.skymapper.query("SELECT column_name, column_order, description, unit \
                                     FROM TAP_SCHEMA.columns \
                                     WHERE table_name='dr1.master' \
                                     AND column_name='object_id' \
                                     ORDER BY column_order")
-        
+
         expected = [{ 'column_name': 'object_id',
                       'column_order': 1,
                       'description': 'Global unique object ID in the master table.',
                       'unit': 'NULL'}]
-        
+
         self.assertEqual(result,expected)
+
+    # find a better way of testing this (maybe with fixtures)
+    # maybe change the function
+    def test_get_schema(self):
+        result = self.skymapper.get_schema('dr1.master')
+
+    def test_get_batch(self):
+        result = self.skymapper.get_batch('dr1.master', 'object_id', 1, 11, 'object_id')
+
+        expected = [    {'object_id': 1},
+                        {'object_id': 2},
+                        {'object_id': 3},
+                        {'object_id': 4},
+                        {'object_id': 5},
+                        {'object_id': 6},
+                        {'object_id': 7},
+                        {'object_id': 8},
+                        {'object_id': 9},
+                        {'object_id': 10}]
+
+        self.assertEqual(result, expected)
