7e8de4f3030ca9c019759caa3e305a89bab4b342
chrishop <christopher.hopkins@bbc.co.uk>
2020-01-30 21:28:59 +0000

    config to ensure version consistency and install script

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'tap-dat',
+    version = '0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires = [
+        'click',
+        'astropy', 
+        'numpy', 
+        'mysql-connector-python'
+    ],
+    entry_points={
+        'console_scripts': [
+            'tap-dat = tap_dat.__main__:main'
+        ]
+    }
+)
